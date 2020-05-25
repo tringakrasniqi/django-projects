90e898f5482ca8a9f0fbbcb8ddfa5938bc065530
tringak <tringaakrasniqi@gmail.com>
2020-05-25 23:48:16 +0200

    Setup of random generator app

--- dojo_survey/main/main/settings.py
+++ dojo_survey/main/main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.dojo_survey',
+    'apps.random_word_generator',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
