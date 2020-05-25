ce4bab47a6500413c799694cc68a57c7e3881536
tringak <tringaakrasniqi@gmail.com>
2020-05-25 21:52:37 +0200

    fixed checkbox bug

--- dojo_survey/main/apps/dojo_survey/views.py
+++ dojo_survey/main/apps/dojo_survey/views.py
@@ -4,11 +4,11 @@ def index(request):
       return render(request, 'index.html')
 
 def result(request):
-      print(request.POST)
+      print(request.POST.getlist('language'))
       context = {
             "name" : request.POST['name'],
             "location" : request.POST['location'],
-            "language" : request.POST['language'],
+            "language" : request.POST.getlist('language'),
             "comment" : request.POST['comment']
       }
       return render(request, "result.html", context)
