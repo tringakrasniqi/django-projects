50f641d1c89c18ed0ca51487f290fef670bf8b95
tringak <tringaakrasniqi@gmail.com>
2020-05-25 21:01:55 +0200

    added templates and form handling

--- dojo_survey/main/apps/dojo_survey/views.py
+++ dojo_survey/main/apps/dojo_survey/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-      pass
+      return render(request, 'index.html')
+
+def result(request):
+      print(request.POST)
+      context = {
+            "name" : request.POST['name'],
+            "location" : request.POST['location'],
+            "language" : request.POST['language'],
+            "comment" : request.POST['comment']
+      }
+      return render(request, "result.html", context)
