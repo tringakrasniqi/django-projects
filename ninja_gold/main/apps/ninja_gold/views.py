8f1aa0154d9270555c6475bd90c426ad4424515e
tringak <tringaakrasniqi@gmail.com>
2020-05-28 17:05:12 +0200

    added main functionality (proccess_money) and activity section

--- ninja_gold/main/apps/ninja_gold/views.py
+++ ninja_gold/main/apps/ninja_gold/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+import random
+from time import strftime, localtime
 
 def index(request):
       if not 'gold' in request.session:
@@ -9,6 +11,47 @@ def index(request):
       return render(request, 'index.html')
 
 def process_money(request):
-      request.session['gold'] += 10
-      print(request.POST)
+      place = request.POST['place']
+      gold_amount = calculate_gold(place)
+      timestamp = strftime("%A %d %B %Y- %H:%M %Z", localtime())
+      data = get_text(gold_amount)
+
+      request.session['gold'] += gold_amount
+      request.session['activity'].append({
+            'text' : f"{data['text']} from the {place}! ({timestamp})",
+            'earned' : data['earned']
+      })
+      return redirect('/')
+
+
+def calculate_gold(place):
+       # cave
+      minNumber = 5
+      maxNumber = 10
+      
+      if place == 'house':
+            minNumber = 2
+            maxNumber = 5
+      elif place == 'farm': 
+            minNumber = 10
+            maxNumber = 20
+      elif place == 'casino':
+            minNumber = -50 
+            maxNumber = 50
+      goldEarned = random.randint(minNumber, maxNumber)
+      return goldEarned
+
+
+def get_text(amount):
+      values = {}
+      if amount > 0:
+            values['text'] = f"Earned {amount}"
+            values['earned'] = True
+      else:
+            values['text'] = f"Lost {abs(amount)}"
+            values['earned'] = False
+      return values
+
+def reset(request):
+      request.session.flush()
       return redirect('/')
