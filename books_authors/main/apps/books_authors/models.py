b25aa2968a8ce9287f0b87253d3377f324f70224
tringak <tringaakrasniqi@gmail.com>
2020-06-02 23:05:01 +0200

    made migrations

--- books_authors/main/apps/books_authors/models.py
+++ books_authors/main/apps/books_authors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+      title = models.CharField(max_length=255)
+      desc = models.TextField()
+      created_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
+
+class Author(models.Model):
+      first_name = models.CharField(max_length=45)
+      last_name = models.CharField(max_length=45)
+      created_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
+      note = models.TextField()
+      books = models.ManyToManyField(Book, related_name="publishers")
+
