b68836111d48779812cf6423c9d185511d94e5a1
tringak <tringaakrasniqi@gmail.com>
2020-06-06 19:24:39 +0200

    added show_one for author/books and show_all for authors

--- books_authors/main/apps/books_authors/urls.py
+++ books_authors/main/apps/books_authors/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('process_book', views.add_book),
+    path('show_book/<int:book_id>', views.show_book),
+    path('authors', views.show_authors),
+    path('process_author', views.add_author),
+    path('show_author/<int:author_id>', views.show_author)
 ]
--- books_authors/main/apps/books_authors/views.py
+++ books_authors/main/apps/books_authors/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book, Author
 
 def index(request):
-      pass
+      context = {
+            'all_books': Book.objects.all()
+      }
+      return render(request, 'index.html', context)
+
+def add_book(request):
+      Book.objects.create(title=request.POST['title'], desc=request.POST['description'])
+      return redirect('/')
+
+def show_book(request, book_id):
+      context = {
+            'book' : Book.objects.get(id=book_id)
+      }
+      return render(request, 'book_details.html', context)
+
+def show_authors(request):
+      context = {
+            'authors' : Author.objects.all()
+      }
+      return render(request, 'show_authors.html', context)
+
+def add_author(request):
+      Author.objects.create(first_name=request.POST['first_name'], last_name = request.POST['last_name'], note= request.POST['note'])
+      return redirect('/authors')
+
+def show_author(request, author_id):
+      context = {
+            'author': Author.objects.get(id = author_id)
+      }
+      return render(request, 'show_one_author.html', context)
